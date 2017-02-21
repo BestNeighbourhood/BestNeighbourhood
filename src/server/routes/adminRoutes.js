ad29c263f46f15b80c5604f2de690e12ed87f5be
Olga Belavina <ol.belavina@gmail.com>
2017-02-21 08:00:12 -0800

    Refractoring and error tracking

--- src/server/routes/adminRoutes.js
+++ src/server/routes/adminRoutes.js
@@ -12,15 +12,18 @@ var dataService = require('../services/dataService')();
 // DB Models / Schemas
 var Category = require('../models/categoryModel').Category;
 var DsInfo   = require('../models/categoryModel').DsInfo;
-
 var dataSetSchema  = require('../models/dsSchema');
+
+// Load borders 
+var nbrBorders = require('../data/list.json');
+var inside = require('point-in-polygon');
 
 var router = function() {
 
     adminRouter.route('/loadTestDataSet').get(function (req, res) {
-        dataService.selectDataFromDataSet( "ea23ff2c-cc42-4fea-8df3-1677b35538cf", function (err, dataSetInfo) {
-                console.log(dataSetInfo);
-                res.send(dataSetInfo);
+        dataService.selectDataFromDataSet( "ea23ff2c-cc42-4fea-8df3-1677b35538cf", "en-1", function (err, dataSetInfo) {
+            console.log(dataSetInfo);
+            res.send(dataSetInfo);
         });
     });
 
@@ -32,19 +35,19 @@ var router = function() {
 
                 // Drop existing collections first :
                 Category.remove({}, function (err) {
-                    console.log("✔-x-- Categories dropped!");
+                    logger.info("✔-x-- Categories dropped!");
                 });
 
                 DsInfo.remove({}, function (err) {
-                    console.log("✔-x-- DatasetInfo dropped!");
+                    logger.info("✔-x-- DatasetInfo dropped!");
                 });
 
                 Category.create(categories, function(err, results) {
                     if (err) {
-                        console.log('X-- Error while adding categories: ' + err);
+                        logger.info('X-- Error while adding categories: ' + err);
                     } else {
-                        console.log("✔-- Categories in db!");
-                        console.log("✔-- DataSet info [items] within categories in db!");
+                        logger.info("✔-- Categories in db!");
+                        logger.info("✔-- DataSet info [items] within categories in db!");
 
                         // Fetch data Sets
                         var cursor = Category.find().cursor();
@@ -52,49 +55,57 @@ var router = function() {
 
                             var dsIdsCount = doc.items.length;
                             for (var i = 0; i < dsIdsCount; i++) {
-                               var dataSetId = doc.items[i].data_set_uuid;
+                                var dataSetId = doc.items[i].data_set_uuid;
 
-                               dataService.getDataSetInfo(dataSetId, function (err, dsInfo) {
-                                   
-                                   var collectionTitle = dsInfo['data_set_metas'][0]['title'];
-                                   var collectionVersion = dsInfo['versions'][0]['identifier'];
-                                   var category = doc.title;
-                                   var datasetInfo = {
-                                       id : dsInfo.id,
-                                       title : collectionTitle,
-                                       version : collectionVersion,
-                                       category : category
-                                   };
+                                dataService.getDataSetInfo(dataSetId, function (err, dsInfo) {
+
+                                    if (err) {
+                                        logger.warn('X-- Error occured while fetching dataset info "' + dataSetId + '" in a category "' + doc.title + '"');
+                                        logger.warn('X-- ' + err);
+                                        return;
+                                    }
+                                    
+                                    var collectionTitle = dsInfo['data_set_metas'][0]['title'];
+                                    var collectionVersion = dsInfo['versions'][0]['identifier'];
+                                    var category = doc.title;
+                                    var datasetInfo = {
+                                        id : dsInfo.id,
+                                        title : collectionTitle,
+                                        version : collectionVersion,
+                                        category : category
+                                    };
 
                                     DsInfo.create(datasetInfo, function (err, results) {
                                         if (err) {
-                                            console.log('X-- Error while adding DsInfo: ' + err);
+                                            logger.warn('X-- Error while adding DsInfo: ' + err);
+                                            return;
                                         } else {
                                             dataService.selectDataFromDataSet(results.id , results.version, function (err, dataSetData) {
+                                                if (err) {
+                                                    logger.warn('X-- Error occured while fetching dataset "' + dataSetId + '" in a category "' + doc.title + '"');
+                                                    logger.warn('X-- ' + err);
+                                                    return;
+                                                }
                                                 var Ds = connection.model(collectionTitle, dataSetSchema);
                                                 Ds.create(dataSetData, function(err, results) {
                                                     if (err) {
-                                                        console.log('-X-- Error while adding datasets: ' + err);
+                                                        logger.warn('-X-- Error while adding datasets: ' + err);
                                                     } else {
-                                                        console.log("-✔-- Dataset '" + collectionTitle + "'in db!");
-                                                        // console.log(results);
-                                                    }
+                                                        logger.info("-✔-- Dataset '" + collectionTitle + "'in db!");
+                                                    } 
                                                 });
                                             });
-                                        }
+                                         }
                                     });
-                               }); 
+                                }); 
                             }
                         });
                         cursor.on('close', function() {
-                            // Called when done
+                            logger.info("✔-- Done adding datasets in db!");
+                            res.send('Done fetching the results');
                         });
-                        res.send(results);
-                    }
-                }); 
-
-                // res.send("done");
-              //  res.send(catSchema);
+                }
+            }); 
         });
     });
 
--- src/server/services/dataService.js
+++ src/server/services/dataService.js
@@ -1,15 +1,43 @@
 var http = require('http');
+//Logger
+var logger = require('../config/logger');
 
 var dataService = function () {
 
     var api_key = "98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8";
     var organization = "58090332de44093525000062";
-
     var host    = 'api.namara.io';
 
-    // https://api.namara.io/v0/data_sets/b746b8d5-8149-4681-83ca-3688227f2406?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
+    /**
+     *  Wrapper function for http request
+     *  
+     */
+    var makeRequest = function (options, cb) {
+
+        var callback = function (response) {
+            var str = '';
+
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                var errMsg = 'Failed to execute http request, client returned with a status code : ' + response.statusCode;
+                cb(errMsg, '');
+            }
+
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+            response.on('end', function() {
+                 cb(null,  JSON.parse(str));
+            }); 
+        }
+
+        http.request(options, callback).end();
+    };
+
     /**
      *  Get Info about one dataset, to get title - > ['data_set_metas']['title']
+     *  
+     *  e.g. :
+     *  https://api.namara.io/v0/data_sets/b746b8d5-8149-4681-83ca-3688227f2406?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
      */
     var getDataSetInfo = function (dataSetId, cb) {
 
@@ -18,76 +46,40 @@ var dataService = function () {
             path : '/v0/data_sets/' + dataSetId + '?api_key=' + api_key
         };
 
-        var callback = function (response) {
-            var str = '';
-
-            response.on('data', function(chunk) {
-                str += chunk;
-            });
-            response.on('end', function() {
-                 cb(null,  JSON.parse(str));
-            }); 
-        }
-
-        http.request(options, callback).end();
+        makeRequest(options, cb);
     };
 
     /**
      *  Gets list of categories (projects within organization)
      *  To get category title -> [i]['title'], to get dataSets within category [i]['items']
+     * 
+     *  e.g. :
+     *   http://api.namara.io/v0/organizations/58090332de44093525000062/projects?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
      */
     var getListOfCategories = function (cb) {
-
-        //  http://api.namara.io/v0/organizations/58090332de44093525000062/projects?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
     
         var options = {
             host : host,
             path : '/v0/organizations/' + organization + '/projects?api_key=' + api_key
         };
 
-        var callback = function (response) {
-            var str = '';
-
-            response.on('data', function(chunk) {
-                str += chunk;
-            });
-            response.on('end', function() {
-
-                 cb(null, JSON.parse(str));
-            }); 
-        }
-
-        http.request(options, callback).end();
+        makeRequest(options, cb);
     };
 
     /** 
      * Fetch data from a particular dataSet
-     * 
+     *  
+     *  e.g. :
+     *  https://api.namara.io/v0/data_sets/ea23ff2c-cc42-4fea-8df3-1677b35538cf/data/en-1?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
      */
     var selectDataFromDataSet = function (dataSetId, dataSetVersion, cb) {
 
-        var http = require('http');
-
-        // https://api.namara.io/v0/data_sets/ea23ff2c-cc42-4fea-8df3-1677b35538cf/data/en-1?api_key=98c561bd55188933cb81609c0ae4541e87f3f2eb5e86e4e5678bf6a4d8da0fd8
         var options = {
             host : host,
             path : '/v0/data_sets/' + dataSetId + '/data/' + dataSetVersion + '?api_key=' + api_key
         }
 
-
-        var callback = function (response) {
-            var str = '';
-
-            response.on('data', function(chunk) {
-                str += chunk;
-            });
-            response.on('end', function() {
-                 cb(null,  JSON.parse(str));
-            });      
-        };
-
-        http.request(options, callback).end();
-
+        makeRequest(options, cb);
     };
 
     return {
